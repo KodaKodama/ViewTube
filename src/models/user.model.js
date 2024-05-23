489f2bdfd18e89fc737635bbdfa2fb6628b6773e
kodakodama <163364396+KodaKodama@users.noreply.github.com>
2024-05-23 04:51:17 -0700

    Add methods to generate access and refresh tokens using jwt

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,5 +1,6 @@
 import mongoose, {Schema} from "mongoose";
 import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
 
 const userSchema = new Schema({
     usernamee: {
@@ -17,7 +18,7 @@ const userSchema = new Schema({
         lowercase: true,
         trim: true,
     },
-    fullnamee: {
+    fullName: {
         type: String,
         required: true,
         trim: true,
@@ -52,6 +53,33 @@ userSchema.pre("save", async function(next){
 
 userSchema.methods.isPasswordCorrect = async function(password){
     return await bcrypt.compare(password, this.password);
+}
+
+userSchema.methods.generateAccessToken = function(){
+  return jwt.sign(
+        {
+            _id: this.id,
+            email: this.email,
+            usuername: this.usernamee,
+            fullName: this.fullName
+        },
+        process.env.SECRET_TOKEN,
+        {
+            expiresIn: process.env.ACCESS_TOKEN_EXPIRY
+        }
+    )
+}
+
+userSchema.methods.generateRefreshToken = function(){
+    return jwt.sign(
+        {
+            _id: this.id
+        },
+        process.env.REFRESH_TOKEN_SECRET,
+        {
+            expiresIn: process.env.REFRESH_TOKEN_EXPIRY
+        }
+    )
 }
 
 export const User = mongoose.model("User", userSchema)
